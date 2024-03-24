2ba127b5b1aa24f522f8c30fff4997b1f7509ef7
Sadheera Madhushan <gitzdsproj@gmail.com>
2024-03-24 16:18:22 +0530

    TT-001: sixth commit

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -1,12 +1,13 @@
 package org.example;
+
 public class First {
 
     private int id;
 
     private String name;
 
-    public First(int id, String name) {
-        this.id = id;
+    // constructor
+    public First(String name) {
         this.name = name;
     }
 }

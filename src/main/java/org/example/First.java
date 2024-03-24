bd298a90d14b4a6419520da8cd7d0411f4031fb7
Sadheera Madhushan <gitzdsproj@gmail.com>
2024-03-24 15:55:29 +0530

    TT-001: third commit

---
+++ src/main/java/org/example/First.java
@@ -0,0 +1,12 @@
+package org.example;
+public class First {
+
+    private int id;
+
+    private String name;
+
+    public First(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}

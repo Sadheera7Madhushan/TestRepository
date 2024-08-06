060f1b82d32e03491852c50276fc61ea247ef22e
Sadheera Madhushan <Sadheera.Madhushan-ms@dialog.lk>
2024-08-06 22:51:27 +0530

    third commit

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -1,9 +1,12 @@
 package org.example;
+
 public class First {
 
     private int id;
 
     private String name;
+
+    private String address;
 
     public First(String name) {
         this.name = name;
